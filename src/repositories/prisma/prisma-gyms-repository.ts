cc08de44b5aab9a77c446a4362a4d5a472fce6d6
Gabriel <gabrielhenrique.ls96@gmail.com>
2023-04-14 19:04:22 -0300

    add factories

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -2,7 +2,7 @@ import { prisma } from '@/lib/prisma'
 import { Gym, Prisma } from '@prisma/client'
 import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 
-export class PrismaGymsRepositoy implements GymsRepository {
+export class PrismaGymsRepository implements GymsRepository {
   async findById(id: string) {
     const gym = await prisma.gym.findUnique({
       where: {
