4f96a7c4f1ae973cf6e32110b4e5008076270118
Gabriel <gabrielhenrique.ls96@gmail.com>
2023-04-17 17:28:40 -0300

    Add gyms e2e tests

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -4,10 +4,10 @@ import { z } from 'zod'
 
 export async function nearby(request: FastifyRequest, reply: FastifyReply) {
   const nearbyGymsQuerySchema = z.object({
-    latitude: z.number().refine((value) => {
+    latitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 90
     }),
-    longitude: z.number().refine((value) => {
+    longitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 180
     }),
   })
@@ -16,10 +16,10 @@ export async function nearby(request: FastifyRequest, reply: FastifyReply) {
 
   const fetchNearbyGymsService = makeFetchNearbyGymsService()
 
-  await fetchNearbyGymsService.handle({
+  const { gyms } = await fetchNearbyGymsService.handle({
     userLatitude: latitude,
     userLongitude: longitude,
   })
 
-  return reply.status(201).send()
+  return reply.status(200).send({ gyms })
 }
