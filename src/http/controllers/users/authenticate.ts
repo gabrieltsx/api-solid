8c4dd1c8bb34f445697efa074c9c5e43b62d121c
Gabriel <gabrielhenrique.ls96@gmail.com>
2023-04-17 23:01:13 -0300

    Add refresh-token and RBAC

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -22,9 +22,25 @@ export async function authenticate(
       password,
     })
 
-    const token = await reply.jwtSign({}, { sign: { sub: user.id } })
+    const token = await reply.jwtSign(
+      { role: user.role },
+      { sign: { sub: user.id } },
+    )
 
-    return reply.status(200).send({ token })
+    const refreshToken = await reply.jwtSign(
+      { role: user.role },
+      { sign: { sub: user.id, expiresIn: '7d' } },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({ token })
   } catch (err) {
     if (err instanceof InvalideCredentialsError) {
       return reply.status(409).send({ message: err.message })
